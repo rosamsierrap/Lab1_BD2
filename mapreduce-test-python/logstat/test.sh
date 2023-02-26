3ca9b75f0743f89e399979dbe6064e72fb1b11e8
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-02-26 13:49:43 -0500

    Update test.sh

--- mapreduce-test-python/logstat/test.sh
+++ mapreduce-test-python/logstat/test.sh
@@ -3,7 +3,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../data/access.log /logstat/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file ../../mapreduce-test-python/logstat/mapper.py -mapper ../../mapreduce-test-python/logstat/mapper.py \
 -file ../../mapreduce-test-python/logstat/reducer.py -reducer ../../mapreduce-test-python/logstat/reducer.py \
